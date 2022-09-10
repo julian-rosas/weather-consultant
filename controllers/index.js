b96ca206315d3e36ef35e191a5d96f7f386f041e
Ricardo Flores Mata <ricardo_fm77@ciencias.unam.mx>
2022-09-10 14:51:29 -0500

    views ready

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,24 @@ const index = (req, res, next) => {
     });
 };
 
+const viewWeather = (req, res, next) => {
+    res.render('clima', {
+        title: 'Clima'
+    });
+};
+
+const getWeather = (req, res, next) => {
+    let s = req.body.iataCodeOrigen;
+    let q = req.body.iataCodeDestiny;
+    console.log(s);
+    console.log(q);
+    res.redirect('/viewweather');
+};
+
+
+
 module.exports = {
-    index
+    index,
+    viewWeather,
+    getWeather
 }
